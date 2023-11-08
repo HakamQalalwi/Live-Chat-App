a0eceb91b4fb409957a79214e0d1d9b426c659f5
Hakam Qalalwi <hakamqalalwi@outlook.com>
2023-11-09 02:27:49 +0300

    create navBar section and fix errors

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,56 @@
+import { useContext } from "react";
+import { Container, Nav, Navbar, Stack } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import Notification from "./chat/Notification";
+
+const NavBar = () => {
+  const { user, logoutUser } = useContext(AuthContext);
+
+  return (
+    <Navbar bg="dark" className="mb-4" style={{ height: "3.75rem" }}>
+      <Container>
+        <h2>
+          <Link to="/" className="link-light text-decoration-none">
+            Live Chat App
+          </Link>
+        </h2>
+        {user && (
+          <span className="text-warning">Logged in as {user?.name}</span>
+        )}
+        <Nav>
+          <Stack direction="horizontal" gap={3}>
+            {user && (
+              <>
+                <Notification />
+                <Link
+                  onClick={() => logoutUser()}
+                  to="/login"
+                  className="link-light text-decoration-none"
+                >
+                  Logout
+                </Link>
+              </>
+            )}
+
+            {!user && (
+              <>
+                <Link to="/login" className="link-light text-decoration-none">
+                  Login
+                </Link>
+                <Link
+                  to="/register"
+                  className="link-light text-decoration-none"
+                >
+                  Register
+                </Link>
+              </>
+            )}
+          </Stack>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
