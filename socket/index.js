4ebc824906803a438b6a29fb6ed4ca40b426c736
Hakam Qalalwi <hakamqalalwi@outlook.com>
2023-11-09 02:56:03 +0300

    Activate socket system

---
+++ socket/index.js
@@ -0,0 +1,48 @@
+const { disconnect } = require("mongoose");
+const { Server } = require("socket.io");
+
+const io = new Server({
+  cors: "http://localhost:5173/",
+});
+
+let onlineUsers = [];
+
+io.on("connection", (socket) => {
+  console.log("new connection", socket.id);
+
+  // listen to a connection
+  socket.on("addNewUser", (userId) => {
+    !onlineUsers.some((user) => user.userId === userId) &&
+      onlineUsers.push({
+        userId,
+        socketId: socket.id,
+      });
+
+    console.log("OnlineUsers", onlineUsers);
+
+    io.emit("getOnlineUsers", onlineUsers);
+  });
+
+  //add message
+  socket.on("sendMessage", (message) => {
+    const user = onlineUsers.find(
+      (user) => user.userId === message.recipientId
+    );
+
+    if (user) {
+      io.to(user.socketId).emit("getMessage", message);
+      io.to(user.socketId).emit("getNotification", {
+        senderId: message.senderId,
+        isRead: false,
+        date: new Date(),
+      });
+    }
+  });
+  socket.on("disconnect", () => {
+    onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+
+    io.emit("getOnlineUsers", onlineUsers);
+  });
+});
+
+io.listen(3000);
